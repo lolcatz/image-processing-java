1595b5c5c16fdec0a2feba26d4e716327f2f02a8
Paul Saikko <paul.saikko@cs.helsinki.fi>
2012-03-22 20:49:55 +0200

    suddenly netbeans project

---
+++ src/Barrier.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.concurrent.Semaphore;
+
+/**
+ *
+ * @author Paul
+ */
+
+public class Barrier implements Runnable {
+  Semaphore stop;
+  Semaphore[] gates;
+  int threads;
+  int phases;
+  Image helper;
+  Image im;
+
+  boolean finished = false;
+  public Barrier(Semaphore stop, Semaphore[] gates, int threads, int phases,
+            Image im, Image helper) {
+    this.stop = stop;
+    this.gates = gates;
+    this.threads = threads;
+    this.phases = phases;
+    this.im = im;
+    this.helper = helper;
+  }
+
+  public void run() {
+    for (int j = 0; j < phases * 2; j++) {
+      // wait for all of the threads to stop
+      for (int i = 0; i < threads; i++)
+        try { stop.acquire(); } catch (Exception e) {}
+        // release all worker threads
+        for (int i = 0; i < threads; i++)
+        gates[i].release();
+    }
+    finished = true;
+  }
+}
