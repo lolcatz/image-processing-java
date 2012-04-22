ac5dbf938210f421eed0190f0bb01f9e7aa8338b
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2012-04-22 16:11:13 +0300

    split main in to functions

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,32 @@
 import java.util.concurrent.Semaphore;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
 
-  private final String infile;
-  private final String outfile;
-  private final Image helperImage;
-  private final Image image;
+  private String infile;
+  private String outfile;
+  private int threads;
+  private int phases;
 
-  private static void usage() {
-    System.out.print("Usage: java -jar dist/image_processing.jar ");
-    System.out.println("<input file> [output file]");
-    System.out.println("Input file is mandatory, output file is not.");
-  }
+  private Image helperImage;
+  private Image image;
 
   public static void main(String[] args) throws Exception {
-    if (args.length < 1) {
-      usage();
-      System.exit(0);
-    }
-
-    String infile = args[0];
-    String outfile = null;
-
-    System.out.println("Input file: " + infile);
-
-    if (args.length >= 2) {
-      outfile = args[1];
-      System.out.println("Output file: " + outfile);
-    } else {
-      System.out.println("Output won't be saved.");
-    }
-
-    Main main = new Main(infile, outfile);
+    Main main = new Main();
+    main.processImage();
+    main.saveImage();
   }
 
-  private Main(String infile, String outfile) throws Exception {
-    this.infile = infile;
-    this.outfile = outfile;
+  private Main() throws Exception {
+    setProperties();
+  }
+
+  private void processImage() throws Exception {
     this.image = readImage();
     this.helperImage = new Image(this.image.width,
             this.image.height, this.image.maxval);
 
     long t_start = System.nanoTime();
-    // set number of threads and repetitions for operations
-    final int threads = 4;
-    final int phases = 5;
-
-    System.out.println("Processing with "+threads+" threads..");
+    System.out.println("Processing with " + threads + " threads..");
 
     // initialize semaphores for synchronization
     Semaphore stop = new Semaphore(0);
@@ -80,18 +58,30 @@ public class Main {
     long t_end = System.nanoTime();
     System.out.println("Time: "+(t_end - t_start)/1000000 + " ms");
 
-    if (outfile != null) {
-      // save processed image
-      t_start = System.nanoTime();
-      try {
-        this.image.Save(outfile);
-      } catch (Exception ex) {
-        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      t_end = System.nanoTime();
+  }
 
-      System.out.println("Image saved in: "+(t_end - t_start)/1000000 + " ms");
+  private void setProperties() {
+    this.infile = System.getProperty("infile");
+    System.out.println("infile: " + this.infile);
+
+    this.outfile = System.getProperty("outfile");
+    System.out.println("outfile: " + this.outfile);
+
+    try {
+      this.phases = Integer.parseInt(System.getProperty("phases"));
+    } catch (NumberFormatException e) {
+      System.out.println("phases needs to be a number");
+      System.exit(0);
     }
+    System.out.println("phases: " + this.phases);
+
+    try {
+      this.threads = Integer.parseInt(System.getProperty("threads"));
+    } catch (NumberFormatException e) {
+      System.out.println("threads needs to be a number");
+      System.exit(0);
+    }
+    System.out.println("threads: " + this.threads);
   }
 
   private Image readImage() {
@@ -107,6 +97,26 @@ public class Main {
     long t_end = System.nanoTime();
     System.out.println("Image loaded in " +(t_end - t_start)/1000000 + " ms");
     return img;
+  }
+
+  private void saveImage() {
+    if (outfile == null) {
+      System.out.println("output file not specified, not saving.");
+      return;
+    }
+
+    System.out.println("Saving output to " + this.outfile);
+
+    long t_start = System.nanoTime();
+    try {
+      this.image.Save(outfile);
+    } catch (Exception ex) {
+      System.out.println("Saving image failed.");
+      System.out.println(ex);
+    }
+    long t_end = System.nanoTime();
+
+    System.out.println("Image saved in: "+(t_end - t_start)/1000000 + " ms");
   }
 
 }
--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-java -jar dist/image_processing.jar "/home/fs/kerola/rio_testdata/7976x4480.ppm" "data/7976x4480_out.ppm"
+java \
+  -Dinfile=/home/fs/kerola/rio_testdata/7976x4480.ppm \
+  -Doutfile=data/7976x4480_out.ppm \
+  -Dphases=4 \
+  -Dthreads=5 \
+  -jar dist/image_processing.jar
 
